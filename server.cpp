30d6990bec278aba276cd893fb71da0d6f65c0e8
yaoxisheng <yaoxisheng@yao-Lenovo-Y470.(none)>
2014-07-21 09:03:35 -0400

    finish publishing

--- server.cpp
+++ server.cpp
@@ -1,32 +1,23 @@
-#include<arpa/inet.h>
-#include<sys/socket.h>
-#include<sys/types.h>
-#include<netinet/in.h>
-#include<netinet/tcp.h>
-#include<signal.h>
-#include<unistd.h>
-#include<pthread.h>
-#include<stdlib.h>
-#include<memory.h>
-#include<stdio.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <signal.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <openssl/sha.h>
 
 #define MAXLINE 4096
 
-void receiveTorrent(int connfd) {
-	printf("receiving torrent\n");
+using namespace std;
 
-	int n, fileSize; 
-	
-	// recv torrent file   
-	n = recv(connfd, &fileSize, sizeof(int), 0);
-    printf("receive file size: %i\n", fileSize);   
-
-	char* buff = (char*) malloc (fileSize);    
-	n = recv(connfd, buff, fileSize, 0);
-    printf("receive file contents:\n%s", buff);	
-
-	free(buff);
-}
+void receiveTorrent(int connfd, FILE* oFile);
 
 int main(int argc, char* argv[]) {
     int listenfd, connfd;
@@ -47,29 +38,65 @@ int main(int argc, char* argv[]) {
     if (bind(listenfd, (struct sockaddr*)&servaddr, sizeof(servaddr))) {
         printf("bind error\n");
         exit(0);
-    }
+    }    
     
     if (listen(listenfd, 10) < 0) {
         printf("listen error\n");
         exit(0);
     }
     
+    struct sockaddr_in clientAddr;
+    socklen_t addrlen = sizeof(clientAddr);
+    string ip_str;
+    FILE* oFile;
+    
     while (1) {
-        if ((connfd = accept(listenfd, (struct sockaddr*)NULL, NULL)) == -1) {            
+        if ((connfd = accept(listenfd, (struct sockaddr*)&clientAddr, &addrlen)) == -1) {            
             printf("accept error\n");
             continue;
         }
-
+        
+        oFile = fopen("peer_list","w");
+        ip_str = string(inet_ntoa(clientAddr.sin_addr));
+        fprintf(oFile, "%s %s ", ip_str.c_str(), "6881");
+       
         n = recv(connfd, &buff, sizeof(char), 0);
         printf("request command: %c\n", buff);
 		
 		if (buff == 'p') {
-			receiveTorrent(connfd);
+			receiveTorrent(connfd, oFile);
 		}
+		fclose(oFile);
 	
         close(connfd);
     }
     
     close(listenfd);    
     return 0;
+}
+
+void receiveTorrent(int connfd, FILE* oFile) {
+	printf("receiving torrent\n");
+
+	int n, fileSize;
+	unsigned char hash_value[20];
+	
+	// recv torrent file   
+	n = recv(connfd, &fileSize, sizeof(int), 0);
+    printf("receive file size: %i\n", fileSize);   
+
+	char* buff = (char*) malloc (fileSize);	
+	n = recv(connfd, buff, fileSize, 0);
+    printf("receive filex contents:\n%s", buff);
+    
+    // hash torrent file 
+    unsigned char* buff2 = (unsigned char*) malloc(fileSize);
+	memcpy(buff2,buff,fileSize);
+    SHA1(buff2, fileSize, hash_value);
+    for (int i = 0; i < 20; i++) {
+		fprintf(oFile, "%02x" , hash_value[i]);
+	}		
+	fprintf(oFile, "\n");
+
+	free(buff);
 }
