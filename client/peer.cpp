640ea920e71ea070b4c19e6f5bb173de052ab102
yaoxisheng <yaoxisheng@yao-Lenovo-Y470.(none)>
2014-08-02 13:52:10 -0400

    send bitfield, create thread to listen for request

--- client/peer.cpp
+++ client/peer.cpp
@@ -10,18 +10,21 @@
 #include <memory.h>
 #include <stdio.h>
 #include <string.h>
+#include <openssl/sha.h>
+#include <pthread.h>
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 #include <map>
-#include <openssl/sha.h>
 
 using namespace std;
 
+int port_num = 7000;
 map<string, string> hash_map;   // key:info_hash, value:torrent_file_name
 
-void handshake(int connfd, char* info_hash);
 void handle_handshake(int connfd, int length);
+void *listen_for_request(void *arg);
 
 int main(int argc, char* argv[]) {
     // read torrent list
@@ -60,7 +63,8 @@ int main(int argc, char* argv[]) {
 	    delete buffer;
 	    fclose(pFile);
     }
-
+    
+    // listen for handshake
     int listenfd, connfd;
     struct sockaddr_in servaddr;
     char buff;
@@ -116,47 +120,25 @@ int main(int argc, char* argv[]) {
     return 0;
 }
 
-void handshake(int connfd, char* info_hash) {
-	char mes_id = '0';
-	int mes_len = 4 + 1 + strlen(info_hash);    
-
-	if (send(connfd, &mes_len, sizeof(uint32_t), 0) < 0) {
-        printf("send error\n");
-        exit(0);
-    }	
-
-	if (send(connfd, &mes_id, sizeof(char), 0) < 0) {
-        printf("send error\n");
-        exit(0);
-    }
-    
-	if (send(connfd, info_hash, strlen(info_hash), 0) < 0) {
-        printf("send error\n");
-        exit(0);
-    }
-
-	printf("Initiate handshake in connfd%i, content: %i%s", connfd, mes_len, info_hash);  
-}
-
-void handle_handshake(int connfd, int length) {
+void handle_handshake(int connfd, int length) {    
     int n;
-    char* hash_info = new char[length];
+    char* info_hash = new char[length];
     
-    n = recv(connfd, hash_info, length - 1, 0);
-    hash_info[length - 1] = '\0';
+    n = recv(connfd, info_hash, length - sizeof(int) - 1, 0);
+    info_hash[length - 1] = '\0';
     
-    // search hash_info from hash_map
-    string hash_info_str(hash_info);   
-    if(hash_map.find(hash_info) == hash_map.end()) {
-        // hash_info not found
+    // search info_hash from hash_map
+    string info_hash_str(info_hash);
+    if(hash_map.find(info_hash_str) == hash_map.end()) {        
+        cout << "info_hash not found" << endl;
         return;
     }   
     
     // handshake reply
-    int length2 = 1;
-    char message_id = 'r';
+    int length2 = sizeof(int) + 1;
+    char message_id = '1';
     
-    if (send(connfd, &length2, sizeof(uint32_t), 0) < 0) {
+    if (send(connfd, &length2, sizeof(int), 0) < 0) {
         printf("send error\n");
         exit(0);
     }
@@ -165,4 +147,121 @@ void handle_handshake(int connfd, int length) {
         printf("send error\n");
         exit(0);
     }
+    
+    // send bitfield    
+    string torrent_file_name = hash_map[info_hash_str];
+    ifstream ifs(torrent_file_name);
+    
+    string video_name;
+    string piece_num_str;
+    getline(ifs, video_name);
+    getline(ifs, piece_num_str);
+    ifs.close();
+    
+    istringstream iss(piece_num_str);
+    int piece_num;
+    iss >> piece_num;
+    
+    char bitfield[piece_num];
+    for (int i = 0; i < piece_num; i++) {
+        bitfield[i] = 0;
+    }
+    
+    ifstream ifs2(video_name);
+    if (ifs2) {
+        for (int i = 0; i < piece_num; i++) {
+            bitfield[i] = 1;
+        }
+        ifs2.close();
+    } else {        
+        string video_name_prefix = video_name.substr(0, video_name.find('.'));
+        ostringstream oss;
+        for (int i = 0; i < piece_num; i++) {
+            oss << i;
+            string video_piece = video_name_prefix + '_' + oss.str() + ".avi";
+            ifstream ifs3(video_piece);
+            if (ifs3) {
+                bitfield[i] = 1;
+            }
+            oss.str("");
+        }
+    }
+    
+    int length3 = sizeof(int) + 1 + piece_num + sizeof(int);
+    char message_id2 = '2';
+    
+    if (send(connfd, &length3, sizeof(int), 0) < 0) {
+        printf("send error\n");
+        exit(0);
+    }
+    
+    if (send(connfd, &message_id2, sizeof(char), 0) < 0) {
+        printf("send error\n");
+        exit(0);
+    }
+    
+    if (send(connfd, bitfield, piece_num, 0) < 0) {
+        printf("send error\n");
+        exit(0);
+    }
+    
+    if (send(connfd, &port_num, sizeof(int), 0) < 0) {
+        printf("send error\n");
+        exit(0);
+    }
+    
+    // create new thread to listen for request
+    pthread_t thread_id;
+    pthread_create(&thread_id, NULL, listen_for_request, NULL);    
 }
+
+void *listen_for_request(void *arg) {
+    int listenfd, connfd;
+    struct sockaddr_in servaddr;
+    char buff;
+    int n;
+    
+    if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        printf("socket error\n");
+        exit(0);
+    }
+    
+    memset(&servaddr, 0 ,sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(port_num++);
+    
+    if (bind(listenfd, (struct sockaddr*)&servaddr, sizeof(servaddr))) {
+        printf("bind error\n");
+        exit(0);
+    }
+    
+    if (listen(listenfd, 10) < 0) {
+        printf("listen error\n");
+        exit(0);
+    }
+    
+    struct sockaddr_in clientAddr;
+    socklen_t addrlen = sizeof(clientAddr);    
+    
+    while (1) {
+        if ((connfd = accept(listenfd, (struct sockaddr*)&clientAddr, &addrlen)) == -1) {
+            printf("accept error\n");
+            continue;
+        }
+        
+        int length;
+        n = recv(connfd, &length, sizeof(int), 0);      
+        
+        char message_id;
+        n = recv(connfd, &message_id, sizeof(char), 0);
+        
+        // receive request
+	
+        close(connfd);
+    }
+    
+    close(listenfd);
+    return NULL;
+}
+
